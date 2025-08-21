44d1113578449dd8def0d6cd39d5a375df7d4cb9
Name Anna Starenka <starenka.anna.zoolux@gmail.com>
2025-08-21 16:50:57 +0300

    first commit

---
+++ app/meals/share/page.js
@@ -0,0 +1,95 @@
+"use client";
+
+import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useActionState, useState } from "react";
+
+export default function ShareMealPage() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+
+  const [state, formAction] = useActionState(shareMeal, { message: null });
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form} action={formAction}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input
+              type="text"
+              id="summary"
+              name="summary"
+              value={summary}
+              onChange={(e) => setSummary(e.target.value)}
+              required
+            />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              value={instructions}
+              onChange={(e) => setInstructions(e.target.value)}
+              required
+            ></textarea>
+          </p>
+          <ImagePicker label="your image" name="image"/>
+          {state.message && <p>{state.message}</p>}
+          <p className={classes.actions}>
+            <MealsFormSubmit />
+          </p>
+        </form>
+      </main>
+    </>
+  );
+}
